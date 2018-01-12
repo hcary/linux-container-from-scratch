6ecc2466490e7f6c9488d4c3f683db86b5cbb785
Harvey Cary <hcary@rc3labs.com>
2018-01-12 17:29:23 -0500

    Add blanks lines at the end of files to make cat read easier on the eyes

--- src/make-container/iptables.sh
+++ src/make-container/iptables.sh
@@ -7,3 +7,5 @@ iptables -t nat -F
 iptables -t nat -A POSTROUTING -s 172.16.99.100/24 -o enp0s3 -j MASQUERADE
 iptables -A FORWARD -i enp0s3 -o veth0 -j ACCEPT
 iptables -A FORWARD -o enp0s3 -i veth0 -j ACCEPT
+
+
