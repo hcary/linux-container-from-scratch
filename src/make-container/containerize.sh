5160dece57fca3f6d2931d1a684cead344ee826f
Harvey Cary <hcary@rc3labs.com>
2018-01-12 16:43:19 -0500

    Change sheel to bash inside container

--- src/make-container/containerize.sh
+++ src/make-container/containerize.sh
@@ -4,5 +4,5 @@ source vars.sh
 
 # ip netns allows applications that are namespace unware run in a separate network namespace
 ip netns add netns1
-unshare --mount --uts --ipc --net=/var/run/netns/netns1 --pid --fork sh
+unshare --mount --uts --ipc --net=/var/run/netns/netns1 --pid --fork /bin/sh 
 
