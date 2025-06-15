c34f119615c4befb091a23ee955ba746af08076f
Harvey Cary <hcary@Harveys-MacBook-Pro.local>
2025-06-14 22:41:56 -0400

    Migrate to Linux kernel 6.x and python3

---
+++ mem-leak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+#print("PID: " + str(os.getpid()))
+
+with open("/dev/urandom", "rb") as f:
+    data = b""
+    i = 0
+    while True:
+        data += f.read(10_000_000)  # 10 MB
+        i += 1
+        print(f"{i*10}mb")
